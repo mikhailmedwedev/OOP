60f5e8fdb52609b89a0f5c578dcc595ccd789218
mikhailmedwedev <mikhail.medwedev@mail.ru>
2026-09-11 11:35:24 +0700

    Main file added

--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -1,5 +1,18 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
 
+    public static int[] heapsort(int[] arr) {
+        Heap.sort(arr);
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        heapsort(arr);
+        String sortedArr = Arrays.toString(arr);
+        System.out.println(sortedArr);
+    }
 }
