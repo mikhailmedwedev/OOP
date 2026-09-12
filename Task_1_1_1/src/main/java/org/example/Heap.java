59e17216b2f542f8f92a32359a4367cfa4bd2218
mikhailmedwedev <mikhail.medwedev@mail.ru>
2026-09-12 11:53:00 +0700

    javadoc comments

--- Task_1_1_1/src/main/java/org/example/Heap.java
+++ Task_1_1_1/src/main/java/org/example/Heap.java
@@ -1,7 +1,16 @@
 package org.example;
 
+/**
+ * Пирамидальная сортировка
+ */
 public class Heap {
 
+    /**
+     * Просеивает элемент вниз по дереву, если он меньше своих потомков
+     * @param arr массив
+     * @param n размер массива
+     * @param i индекс текущего рассматриваемого элемента
+     */
     public static void siftDown(int[] arr, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
@@ -25,6 +34,10 @@ public class Heap {
         }
     }
 
+    /**
+     * Сортирует по возрастанию
+     * @param arr неотсортированный массив
+     */
     public static void sort(int[] arr) {
         int n = arr.length;
         for (int i = n / 2 - 1; i >= 0; i--) {
--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -2,13 +2,25 @@ package org.example;
 
 import java.util.Arrays;
 
+/**
+ * Запуск пирамидальной сортировки
+ */
 public class Main {
 
+        /**
+     * Сортирует массив
+     * @param arr входной массив
+     * @return отсортированный по возрастанию массив
+     */
     public static int[] heapsort(int[] arr) {
         Heap.sort(arr);
         return arr;
     }
 
+        /**
+     * Создается массив из примера, сортируется, и результат выводится
+     * @param args аргументы командной строки - не используются
+     */
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
         heapsort(arr);
