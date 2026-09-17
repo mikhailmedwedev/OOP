fe03f903125073db4ea2fe820fc39e688af22924
mikhailmedwedev <mikhail.medwedev@mail.ru>
2026-09-17 22:10:03 +0700

    Add Deck class for cards

---
+++ Task_1_1_2/src/main/java/org/example/Deck.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private final List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        reset();
+    }
+
+    public final void reset() {
+        cards.clear();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        if (cards.isEmpty()) {
+            reset();
+            shuffle();
+        }
+
+        return cards.remove(cards.size() - 1);
+    }
+
+    public int cardsLeft() {
+        return cards.size();
+    }
+}
