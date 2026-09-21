1df9a5593b0bce9205f7681f43c9bc9dba6b66ec
mikhailmedwedev <mikhail.medwedev@mail.ru>
2026-09-21 21:34:44 +0700

    checkstyle corrections

--- Task_1_1_2/src/main/java/org/example/BlackJackGame.java
+++ Task_1_1_2/src/main/java/org/example/BlackJackGame.java
@@ -130,23 +130,19 @@ public class BlackJackGame {
             dealerScore++;
             System.out.println("Перебор! Вы проиграли этот раунд. Счет " + playerScore + ":"
                     + dealerScore + " в пользу дилера.");
-        }
-        else if (dealer.isBust()) {
+        } else if (dealer.isBust()) {
             playerScore++;
             System.out.println("У дилера перебор! Вы выиграли раунд! Счет " + playerScore + ":"
                     + dealerScore + " в вашу пользу.");
-        }
-        else if (playerHandScore > dealerHandScore) {
+        } else if (playerHandScore > dealerHandScore) {
             playerScore++;
             System.out.println("Вы выиграли раунд! Счет " + playerScore + ":"
                     + dealerScore + " в вашу пользу.");
-        }
-        else if (dealerHandScore > playerHandScore) {
+        } else if (dealerHandScore > playerHandScore) {
             dealerScore++;
             System.out.println("Дилер выиграл раунд. Счет " + playerScore + ":"
                     + dealerScore + " в пользу дилера.");
-        }
-        else {
+        } else {
             System.out.println("Ничья! Счет " + playerScore + ":" + dealerScore + ".");
         }
     }
