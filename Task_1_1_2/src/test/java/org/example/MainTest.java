d392809ed43f0b08475c6414be40e50998c8cf57
mikhailmedwedev <mikhail.medwedev@mail.ru>
2026-09-21 21:49:41 +0700

    checkstyle corrections again2

--- Task_1_1_2/src/test/java/org/example/MainTest.java
+++ Task_1_1_2/src/test/java/org/example/MainTest.java
@@ -1,8 +1,11 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
 
